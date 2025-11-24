d60c5bf4b40c1b87d7293fb262c8a99697641ca6
Ahmet Karabulut <a.karabulut070@gmail.com>
2025-11-24 13:50:31 +0100

    feat : last version exercise 8

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,33 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import socksGreenImage from './assets/images/socks_green.jpeg'
 import socksBlueImage from './assets/images/socks_blue.jpeg'
+import ProductDisplay from './composents/ProductDisplay.vue';
 
-const product = ref('Socks')
-const brand = ref('Vue Mastery')
+const cart = ref([])
+const premium=ref(true)
+const updateCart=(id)=>{
+  cart.value.push(id)
+}
 
-const image = ref(socksGreenImage)
-const inStock = ref(false)
-  
-const details = ref(['50% cotton', '30% wool', '20% polyester'])
-
-const variants = ref([
-  { id: 2234, color: 'green', image: socksGreenImage },
-  { id: 2235, color: 'blue', image: socksBlueImage },
-])
-
-const cart = ref(0)
-
-const addToCart = () => cart.value += 1
-
-const updateImage = (variantImage) => image.value = variantImage
+const removeFromcart=(id)=>{
+  const index=cart.value.indexOf(id);
+  console.log(index);
+  if(index !== -1){
+    cart.value.splice(index,1)
+  }
+}
 
 </script>
   
 <template>
-  <div class="nav-bar"></div>
-  <div class="cart">Cart({{ cart }})</div>
-  <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
-      </div>
-      <div class="product-info">
-        <h1>{{ product }}</h1>
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-        <ul>
-          <li v-for="detail in details">{{ detail }}</li>
-        </ul>
-        <div 
-          v-for="variant in variants" 
-          :key="variant.id"
-          @mouseover="updateImage(variant.image)"
-          class="color-circle"
-          :style="{ backgroundColor: variant.color }"
-        >
-        </div>
-        <button
-          class="button" 
-          :class="{ disabledButton: !inStock }"
-          :disabled="!inStock"
-          v-on:click="addToCart"
-        >
-          Add to cart
-        </button>
-      </div>
-    </div>
+  <div class="nav-bar">
+    <p>Panier : {{ cart }}</p>
   </div>
+  <div class="cart">Cart({{ cart }})</div>
+
+  <ProductDisplay :premium="premium" @add-to-cart="updateCart"></ProductDisplay>
+
+  
+  
 </template>
---
+++ src/composents/ProductDisplay.vue
@@ -0,0 +1,125 @@
+<script setup>
+import ProductDetails from './ProductDetails.vue'
+import {computed,ref} from 'vue'
+import socksGreenImage from '../assets/images/socks_green.jpeg'
+import socksBlueImage from '../assets/images/socks_blue.jpeg'
+
+const product = ref('Socks')
+const brand = ref('CPNV')
+
+// computed property
+const producttitle= computed (()=>{
+  return `${brand.value} ${product.value}`
+})
+
+//const image = ref(socksGreenImage)
+
+const description=ref("These socs are very comportable")
+//const inStock = ref(false)
+  
+const details = ref(['50% cotton', '30% wool', '20% polyester'])
+
+const variants = ref([
+  { id: 2234, color: 'green', image: socksGreenImage ,quantity:5},
+  { id: 2235, color: 'blue', image: socksBlueImage,quantity:10 },
+])
+
+const selectedVariant=ref(variants.value[0])
+const sizes=ref(['S','M','L','XL'])
+
+
+const image=computed(()=>selectedVariant.value.image)
+const inStock=computed(()=>selectedVariant.value.quantity>0)
+const onSale=ref(true)
+const sale=computed(()=>{
+  return onSale.value ? 'are on sale' : ' are not sale'
+})
+
+//const addToCart = () => cart.value += 1
+
+//const updateImage = (variantImage) => image.value = variantImage
+
+
+
+const url=ref('https://www.happysocks.com/ch/')
+
+const props=defineProps({
+    premium:{
+        type:Boolean,
+        required:true
+    }
+})
+
+const shipping=computed(()=>{
+    if (props.premium){
+        return 'Free'
+    }
+    return 5.99
+})
+
+const emit= defineEmits(['add-to-cart','remove-from-cart'])
+
+function updateVariant(index){
+  selectedVariant.value=variants.value[index]
+  console.log('Selected variant :' , selectedVariant.value)
+}
+
+function addToCart(){
+    emit("add-to-cart",selectedVariant.value.id)
+  
+}
+
+function removeFromcart(){
+    
+  
+  emit("remove-from-cart",selectedVariant.value.id)
+}
+
+</script>
+
+
+<template>
+
+
+    <div class="product-display">
+    <div class="product-container">
+      
+      <div class="product-info">
+        <h1>{{ producttitle }}</h1>
+        <p>{{ sale }}</p>
+        <p>{{ description }}</p>
+        <img v-bind:src="image" :class="{'out-of-stock-img': !inStock}" >
+        <button @click="addToCart"
+        :disabled="!inStock"
+        :class="{disabledButton: !inStock}"
+        >Ajouter au panier</button>
+        <button @click="removeFromcart">Enlever le panier</button>
+
+      </div>  
+
+      <div class="product-image">    </div>
+        
+      <h3>Composition</h3>
+        
+        <ProductDetails :premium="premium" @add-to-cart="updateCart" @remove-from-cart="removeFromcart"></ProductDetails>
+
+        <p>{{ shipping }}</p>
+        <h3>Variants</h3>
+        <div >
+          <ul>
+        <li v-for ="(variant , index) in variants" 
+        :key="variant.id"
+        class="color-circle"
+        :style="{backgroundColor:variant.color}"
+         @mouseover="updateVariant(index)"
+         ></li>
+      </ul>
+        </div>
+        
+      <h3>Sizes</h3>
+      <ul>
+        <li v-for="size in sizes" :key="size">{{ size }}</li>
+      </ul>
+    </div>
+    </div>
+</template>
